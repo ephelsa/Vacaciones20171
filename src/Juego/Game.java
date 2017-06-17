ebc65bc67ce7d9d9bf8b2e7a0ab4a03acf801bc1
Leonardo Pérez <ephelsa@hotmail.com>
2017-06-16 21:52:17 -0500

    Ventana.

--- src/Juego/Game.java
+++ src/Juego/Game.java
@@ -21,6 +21,9 @@ public class Game extends Canvas implements Runnable {
 
     private static final String TITLE = "Ventana <3";
 
+    private static int ups = 0;
+    private static int fps = 0;
+
     private static JFrame window;
     private static Thread thread;
 
@@ -47,8 +50,6 @@ public class Game extends Canvas implements Runnable {
          */
         Game ventana = new Game();
         ventana.startThread();
-        JOptionPane.showMessageDialog(null, "Es que joden compa");
-
     }
 
     private synchronized void startThread() {
@@ -58,6 +59,7 @@ public class Game extends Canvas implements Runnable {
         * Hilos:
         * -Graphics <- Actualiza los gráficos.
          */
+
         isWorking = true;
 
         thread = new Thread(this, "Graphics");
@@ -82,24 +84,62 @@ public class Game extends Canvas implements Runnable {
         /*
         * Se encarga de actualizar todas las variables del juego.
          */
+
+        ups++;
     }
 
     private void toShow() {
         /*
         * Se encarga de mostrar las cosas en el juego.
          */
+
+        fps++;
     }
 
     @Override
     public void run() {
-        while(isWorking) {
-            toUpdate();
-            try {
-                thread.sleep(10000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        /*
+         * NS_FOR_SECOND = Equivalencia de nano segundo a segundo.
+         * UPS_TARGET = Actualizaciones por segundo.
+         * NS_FOR_UPDATE = Cuando nanosegundos transcurre para actualizar.
+         *
+         * delta = Cantidad de tiempo hasta realizar una actualización.
+         * timeElapsed = Tiempo que ha pasado desde que se referenció hasta el inicio.
+         * Todo esto es para actualizar el juego.
+          */
+
+        final int NS_FOR_SECOND = 1000000000;
+        final byte UPS_TARGET = 60;
+        final double NS_FOR_UPDATE = NS_FOR_SECOND / UPS_TARGET;
+
+        long updateReference = System.nanoTime();
+        long countReference = System.nanoTime();
+
+        double timeElapsed;
+        double delta = 0;
+
+        while (isWorking) {
+            final long loopStart = System.nanoTime();
+
+            timeElapsed = loopStart - updateReference;
+            updateReference = loopStart;
+
+            delta += timeElapsed / NS_FOR_UPDATE;
+
+            while (delta >= 1) {
+                toUpdate();
+                delta--;
             }
+
             toShow();
+
+            if (System.nanoTime() - countReference > NS_FOR_SECOND) {
+                // Mostrar los Frames Por Segundo - Actualizaciones Por Segundo
+                window.setTitle(TITLE + " || UPS: " + ups + " || FPS: " + fps);
+                ups = 0;
+                fps = 0;
+                countReference = System.nanoTime();
+            }
         }
     }
 }
