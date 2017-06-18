427d0b0eb9c0d306cb01eee57efdd3e795794773
Leonardo Pérez <ephelsa@hotmail.com>
2017-06-17 19:13:20 -0500

    Screen.

---
+++ src/graphic/Screen.java
@@ -0,0 +1,60 @@
+package graphic;
+
+/**
+ * Created by ephelsa on 16/06/17.
+ */
+public final class Screen {
+
+    private final int width;
+    private final int height;
+
+    public final int[] pixels;
+
+    public Screen(final int width, final int height) {
+        this.width = width;
+        this.height = height;
+
+        pixels = new int[width * height];
+    }
+
+    public void cleanScreen() {
+        /** Este método se encarga de limpiar lo que había antes en pantalla
+         *  para poder dibujar de nuevo.
+         *  Si no se limpia, los gráficos van dejando manchas, o un destello detrás de ellos.
+         */
+
+        for (int i = 0; i < pixels.length; i++) {
+            pixels[i] = 0;
+        }
+    }
+
+    public void showScreen(final int compensationX, final int compensationY) {
+        /** Dibujar en pantalla
+         * Después de estar limpia, este método se encarga de dibujar.
+         */
+
+        for (int y = 0; y < height; y++) {
+            int posY = y + compensationY;
+
+            if (posY < 0 || posY >= height) {
+                /* Gestiona que no se salga de la ventana.
+                 * "continue" hace que pase directamente al otro for.
+                 */
+                continue;
+            }
+
+            for (int x = 0; x < width; x++) {
+                int posX = x + compensationX;
+
+                if (posX < 0 || posX >= width) {
+                /* Gestiona que no se salga de la ventana.
+                 * "continue" hace que pase directamente al otro for.
+                 */
+                    continue;
+                }
+
+                // Código para dibujar en pantalla.
+            }
+        }
+    }
+}
